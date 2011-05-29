1c6afcdd68e873361c877f98887f6abed0fd0a44
Colin Woodbury <colingw@gmail.com>
2011-05-29 16:16:52 +0900

    Added strhelp and pghelp
    - strhelp -> String libraries
    - pghelp  -> Help for pygame games.

---
+++ pghelp.py
@@ -0,0 +1,11 @@
+# file:    pghelp.py
+# author:  Colin Woodbury
+# contact: colingw AT gmail
+# about:   Libraries for helping out pygame creation.
+
+# COLOURS
+def rand_colour():
+    '''Generates a random colour.'''
+    from random import Random
+    r = Random()
+    return (r.randint(0, 255), r.randint(0, 255), r.randint(0, 255))
---
+++ strhelp.py
@@ -0,0 +1,31 @@
+# file:    strhelp.py
+# author:  Colin Woodbury
+# contact: colingw AT gmail
+# about:   A module that helps with string processing.
+
+class NonStringError(TypeError):
+    '''An Error for arguments given that are not strings.'''
+
+def check_type(func):
+    def inner(item):
+        if not isinstance(item, str):
+            raise NonStringError('Non-string passed as an argument.')
+        result = func(item)
+        return result
+    return inner
+    
+@check_type
+def reverse_words(items):
+    '''Reverses the order of words in a string formatted
+    like a sentence, or at least part of one.
+    '''
+    words = items.split()
+    words.reverse()
+    return ' '.join(words)
+
+@check_type
+def reverse_chars(line):
+    '''Reverses the chars in a string.'''
+    line = list(line)
+    line.reverse()
+    return ''.join(line)
